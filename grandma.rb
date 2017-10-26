0d981a43a76b51a6820df4ab8750f366ebccd0cf
Akshay Dhingra <akshay1dhingra@gmail.com>
2017-10-26 21:45:41 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -26,7 +26,7 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANMDA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
-    "NO, NOT SINCE 1983!"
+    "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
   end
